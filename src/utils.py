49752993ad545817139e1634678da04c72ebdf40
Steffen Schotthöfer <steffen.schottthoefer@kit.edu>
2021-04-09 12:06:26 +0200

    added basis computation

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@
 collection of utility functions
 """
 
+import numpy as np
 
 def getTestData():
     return 0
@@ -9,5 +10,42 @@ def getTestData():
 def getTrainingData():
     return 0
 
-def computeMonomialBasis(order, spatialDim, quadWeights):
-    return 0
+### Basis Computation
+def computeMonomialBasis1D(quadPts, polyDegree):
+    """
+    params: quadPts = quadrature points to evaluate
+            polyDegree = maximum degree of the basis
+    return: monomial basis evaluated at quadrature points
+    """
+    basisLen = getBasisSize(polyDegree, 1)
+    nq = quadPts.shape[0]
+    monomialBasis = np.zeros((basisLen, nq))
+
+    for idx_quad in range(0, nq):
+        for idx_degree in range(0, polyDegree + 1):
+            monomialBasis[idx_degree, idx_quad] = np.power(quadPts[idx_quad], idx_degree)
+    return monomialBasis
+
+
+def getBasisSize(polyDegree, spatialDim):
+    """
+    params: polyDegree = maximum Degree of the basis
+            spatialDIm = spatial dimension of the basis
+    returns: basis size
+    """
+
+    basisLen = 0
+
+    for idx_degree in range(0, polyDegree + 1):
+        basisLen += int(
+            getCurrDegreeSize(idx_degree, spatialDim))
+
+    return basisLen
+
+
+def getCurrDegreeSize(currDegree, spatialDim):
+    """
+    Computes the number of polynomials of the current spatial dimension
+    """
+    return np.math.factorial(currDegree + spatialDim - 1) / (
+            np.math.factorial(currDegree) * np.math.factorial(spatialDim - 1))
