9744d5c5ffe50f552348f18197f1e01cc634aa3d
Steffen Schotthöfer <steffen.schottthoefer@kit.edu>
2021-05-04 13:54:58 +0200

    updated runscript and options

--- main.py
+++ main.py
@@ -14,11 +14,13 @@ from src.utils import AnalysisTools
 import numpy as np
 import os
 import tensorflow as tf
-from os import path 
-import pandas as pd 
-import numpy as np 
+from os import path
+import pandas as pd
+import numpy as np
+
 pd.set_option('display.float_format', '{:.2e}'.format)
-import pickle 
+import pickle
+
 
 def main():
     '''
@@ -35,19 +37,17 @@ def main():
     parser = OptionParser()
     parser.add_option("-a", "--architecture", dest="architecture", default=2,
                       help="0 : icnn\n 1 : ecnn\n 2 : both", metavar="AUTHOR")
-    parser.add_option("-b", "--bracket", dest="bracket", default=1,
-                      help="size bracket of network parameters", metavar="BRACKET")
     parser.add_option("-c", "--curriculum", dest="curr", default=0, \
                       help="integer between 0 and 2 for learning curriculum", \
                       metavar="CURRICULUM")
     parser.add_option("-d", "--degreeBasis", dest="degreeBasis", default=1,
                       help="degree of the basis",
                       metavar="DEGREE")
-    parser.add_option("-e", "--evalutation", dest="evaluation", default=1,
+    parser.add_option("-e", "--evaluation", dest="evaluation", default=1,
                       help="evaluation and error analysis", metavar="EVALUATE")
     parser.add_option("-l", "--load", dest="load", default=1,
                       help="0: Dont load weights\n1:Load weights", metavar="LOAD")
-    parser.add_option("-o", "--lossCombi", dest="losses", default=1,
+    parser.add_option("-o", "--optimizationObjective", dest="losses", default=1,
                       help="combination of model losses (objective functions): \n"
                            "1 : [h] \n"
                            "2:  [h,alpha] \n"
@@ -60,7 +60,7 @@ def main():
     parser.add_option("-w", "--width", dest='nWidth', default=10, \
                       help="integer for input shape of dense hidden layers; default 10", \
                       metavar="WIDTH")
-    parser.add_option("-s", "--length", dest='nLength', default=0, \
+    parser.add_option("-x", "--length", dest='nLength', default=0, \
                       help="integer for number of dense hidden layers; default 0", \
                       metavar="LENGTH")
 
@@ -80,7 +80,6 @@ def main():
     options.train = bool(int(options.train))
     options.degreeBasis = int(options.degreeBasis)
     options.evaluation = bool(int(options.evaluation))
-    options.bracket = int(options.bracket)
     options.load = bool(int(options.load))
     options.processingmode = int(options.processingmode)
 
@@ -129,7 +128,7 @@ def main():
 
     ### Choose Network size (as discussed, 1000, 2000, 5000 params) ==> Translate to size bracket (1 = 1000,2 = 2000,3 = 5000)
     # Depending on the size bracket, each network needs to adapt its width and depth (to get the corr. number of trainable parameters)
-    #trainableParamBracket = int(options.bracket)
+    # trainableParamBracket = int(options.bracket)
     losses = int(options.losses)  # [mse(h), mse(alpha), mse(u), mse(flux)]
     # inputDim = int(options.degreeBasis) + 1  # CAREFULL HERE; we adjusted for new net input sizes
     inputDim = int(options.degreeBasis)
@@ -174,14 +173,15 @@ def main():
 
     print("---- Evaluation and error Analysis ----")
     if (options.evaluation == True):
-        #testData = utils.getTestData()  # @Will: Your Script goes here
-        #This should be changed to 'loading' the training data from file
-        
-        trainData_pass = DataClass.make_train_data_wrapper(epsilon,alphaMax,sampleSize)
-        testData_pass = DataClass.make_test_data_wrapper('uniform',[1e-8,8,int(100)],[-70,70,int(1e+03)])
+        # testData = utils.getTestData()  # @Will: Your Script goes here
+        # This should be changed to 'loading' the training data from file
+
+        trainData_pass = DataClass.make_train_data_wrapper(epsilon, alphaMax, sampleSize)
+        testData_pass = DataClass.make_test_data_wrapper('uniform', [1e-8, 8, int(100)], [-70, 70, int(1e+03)])
         print('Done passing Train and Test Data')
         for model in modelList:
-            model.errorAnalysis(trainData_pass,testData_pass,'10','10')  # @Will: Your model error analysis Function goes here
+            model.errorAnalysis(trainData_pass, testData_pass, '10',
+                                '10')  # @Will: Your model error analysis Function goes here
         print("Evalution successfull")
     else:
         print("Evaluation skipped")
@@ -190,51 +190,49 @@ def main():
 
 
 if __name__ == '__main__':
-    
-    tex_dataframe = True 
-    
+
+    tex_dataframe = True
+
     new_dataframe = False
-    
-    run_main = False
-    
+
+    run_main = True
+
     if new_dataframe == True:
         datID = '10'
 
-        netNames = ['L1_S15x2','L1_S15x5','L1_S15x10']
-                    #'L1_S20x2','L1_S20x5','L1_S20x10',\
-                    #'L1_S30x2','L1_S30x5','L1_S30x10']
-        
+        netNames = ['L1_S15x2', 'L1_S15x5', 'L1_S15x10']
+        # 'L1_S20x2','L1_S20x5','L1_S20x10',\
+        # 'L1_S30x2','L1_S30x5','L1_S30x10']
+
         deg = 1
         AT = AnalysisTools('M_N')
-        for domain in ['test','train']:
-            for method in ['ecnn','icnn']:
-                
-                AT.newDF(N = deg,domain = domain,datID = datID,method = method,saveNames = netNames)
-           
+        for domain in ['test', 'train']:
+            for method in ['ecnn', 'icnn']:
+                AT.newDF(N=deg, domain=domain, datID=datID, method=method, saveNames=netNames)
+
     if tex_dataframe:
-        netNames = ['L1_S15x2','L1_S15x5','L1_S15x10']
-                    #'L1_S20x2','L1_S20x5','L1_S20x10',\
-                    #'L1_S30x2','L1_S30x5','L1_S30x10']
-        
-        for method in ['ecnn','icnn']:
-                
-            train_path = 'analysis/raw/results_'+method+'_train_'+'10'+ '.pickle'
-            test_path = 'analysis/raw/results_'+method+'_test_'+'10'+'.pickle'
-            
-            with open(train_path,'rb') as handle:
+        netNames = ['L1_S15x2', 'L1_S15x5', 'L1_S15x10']
+        # 'L1_S20x2','L1_S20x5','L1_S20x10',\
+        # 'L1_S30x2','L1_S30x5','L1_S30x10']
+
+        for method in ['ecnn', 'icnn']:
+
+            train_path = 'analysis/raw/results_' + method + '_train_' + '10' + '.pickle'
+            test_path = 'analysis/raw/results_' + method + '_test_' + '10' + '.pickle'
+
+            with open(train_path, 'rb') as handle:
                 df_train = pickle.load(handle)
-            with open(test_path,'rb') as handle:
+            with open(test_path, 'rb') as handle:
                 df_test = pickle.load(handle)
-                
+
             for name in netNames:
-                df_train[name] = pd.to_numeric(df_train[name],downcast=  'float')
-                df_test[name] = pd.to_numeric(df_test[name],downcast=  'float')
-                
+                df_train[name] = pd.to_numeric(df_train[name], downcast='float')
+                df_test[name] = pd.to_numeric(df_test[name], downcast='float')
+
             df_test = df_test.transpose()
             df_train = df_train.transpose()
-            print('\n Test Frame for '+method+':', df_test.to_latex(),'\n')
-            print('\n Train Frame for '+method+':',df_train.to_latex(),'\n')
-            
+            print('\n Test Frame for ' + method + ':', df_test.to_latex(), '\n')
+            print('\n Train Frame for ' + method + ':', df_train.to_latex(), '\n')
+
     if run_main:
-           
         main()
--- runScript.sh
+++ runScript.sh
@@ -1,3 +1,9 @@
-python main.py --architecture=2\
---
---d 1 -t 1 -e 0 -o 1 -w 10 -s 0 -c 0 -l 1 -p 1
+python main.py \
+--architecture=2 \
+--curriculum=0 \
+--degreeBasis=1 \
+--evaluation=0 \
+--load=0 \
+--optimizationObjective=1 \
+--processingmode=1 \
+--train=1
